fbfbd2001671ace11ae681d6c6b59f7865951b2c
Navvi24 <51583613+Navvi24@users.noreply.github.com>
2019-12-28 16:38:31 +0530

    compleetd
    abc

---
+++ config-cloud.php
@@ -0,0 +1,8 @@
+<?php
+\Cloudinary::config(array(
+  "cloud_name" => "navvi24",
+  "api_key" => "595657791498763",
+  "api_secret" => "svWTgmUNtvPBekOLAvLxh-ZvnbI",
+  "secure" => true
+));
+ ?>
--- index.php
+++ index.php
@@ -1,14 +1,46 @@
 <!DOCTYPE html>
+<?php
+require "vendor\autoload.php";
+require "config-cloud.php";
+if (isset($_POST['submit']))
+{
+  $filename = $_POST['nama'];
+  $slug = $_POST['slug'];
+  $fileupload = $_FILES['file']['name'];
+  $file_tmp = $_FILES['file']['tmp_name'];
+  \Cloudinary\Uploader::upload($file_tmp, array("public_id" => $slug));
+}
+ ?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="/public/styles/style.css">
     <title>Image Upload</title>
+    <style media="screen">
+    form{
+        margin-top: 1em;
+        text-align: center;
+      }
+    </style>
   </head>
   <body>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+    <form method="post" enctype="multipart/form-data">
+    <input type="text" name="nama" placeholder="file name">
+    <input type="text" name="slug">
+    <?php echo cl_image_upload_tag('image_id'); ?>
+    <input type="submit" value="Upload" name="submit">
 </form>
+<hr>
+<?php
+if (isset($_POST['submit']))
+{
+echo cl_image_tag($slug);
+}
+else
+{
+  
+}
+?>
   </body>
 </html>

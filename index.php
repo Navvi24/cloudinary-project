d60426745d952e6da9b81616da8ca684e5daf16c
Navvi24 <51583613+Navvi24@users.noreply.github.com>
2019-12-28 13:22:06 +0530

    Add a  image upload code
    code added successfully

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Image Upload</title>
+  </head>
+  <body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+  </body>
+</html>
